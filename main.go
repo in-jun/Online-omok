a2f6ed8de073a1a021525f674d822b42a6db1b6b
in-jun <h12261226@outlook.kr>
2023-03-13 23:46:45 +0900

    변수명을 명확하게 변경함

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -19,7 +20,7 @@ const black uint8 = 1
 const white uint8 = 2
 const emptied uint8 = 0
 
-type omok_room struct {
+type OmokRoom struct {
 	board_15x15 [225]uint8
 	uesr_1      user
 	uesr_2      user
@@ -39,50 +40,48 @@ type Message struct {
 
 var upgrader = websocket.Upgrader{}
 
-var omok_data [max]omok_room
+var OmokRoomData [max]OmokRoom
 
 func main() {
-	http.HandleFunc("/ws", socket_handler)
-	http.HandleFunc("/omok", index)
+	go ckk()
+	http.HandleFunc("/ws", SocketHandler)
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
-func socket_handler(w http.ResponseWriter, r *http.Request) {
+func SocketHandler(w http.ResponseWriter, r *http.Request) {
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Printf("upgrader.Upgrade: %v", err)
-		socket.Close()
 		return
 	}
-	room_matching(socket)
+	RoomMatching(socket)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", html1)
 }
 
-func room_matching(ws *websocket.Conn) {
+func RoomMatching(ws *websocket.Conn) {
 	for {
 		for i := 0; i < max; i++ {
-			if !omok_data[i].uesr_1.check || !omok_data[i].uesr_2.check {
-				if !omok_data[i].uesr_1.check {
-					omok_data[i].uesr_1.check = true
-					omok_data[i].uesr_1.ws = ws
-					return
-				}
-				if !omok_data[i].uesr_2.check {
-					omok_data[i].uesr_2.check = true
-					omok_data[i].uesr_2.ws = ws
-					omok_data[i].message_handler()
-					return
-				}
+			if !OmokRoomData[i].uesr_1.check {
+				OmokRoomData[i].uesr_1.check = true
+				OmokRoomData[i].uesr_1.ws = ws
+				return
+			}
+			if !OmokRoomData[i].uesr_2.check {
+				OmokRoomData[i].uesr_2.check = true
+				OmokRoomData[i].uesr_2.ws = ws
+				OmokRoomData[i].MessageHandler()
+				return
 			}
 		}
 	}
 }
 
-func (room *omok_room) message_handler() {
-	if room.uesr_1.writing("", "", "black", "") || room.uesr_2.writing("", "", "white", "") {
+func (room *OmokRoom) MessageHandler() {
+	if !room.uesr_1.writing("", "", "black", "") || !room.uesr_2.writing("", "", "white", "") {
 		room.reset()
 		return
 	}
@@ -96,21 +95,21 @@ func (room *omok_room) message_handler() {
 		i1, _ := strconv.Atoi(string(m1))
 		if room.board_15x15[i1] == emptied {
 			room.board_15x15[i1] = black
-			if room.uesr_1.writing(string(m1), "black", "", "") || room.uesr_2.writing(string(m1), "black", "", "") || room.victory_Confirm(i1) {
+			if !room.uesr_1.writing(string(m1), "black", "", "") || !room.uesr_2.writing(string(m1), "black", "", "") || room.VictoryConfirm(i1) {
 				room.reset()
 				return
 			}
 		}
 		_, m2, err := room.uesr_2.ws.ReadMessage()
 		if err != nil {
-			log.Printf("conn.ReadMessage: %v", err)
 			room.reset()
+			log.Printf("conn.ReadMessage: %v", err)
 			return
 		}
 		i2, _ := strconv.Atoi(string(m2))
 		if room.board_15x15[i2] == emptied {
 			room.board_15x15[i2] = white
-			if room.uesr_2.writing(string(m2), "white", "", "") || room.uesr_1.writing(string(m2), "white", "", "") || room.victory_Confirm(i2) {
+			if !room.uesr_2.writing(string(m2), "white", "", "") || !room.uesr_1.writing(string(m2), "white", "", "") || room.VictoryConfirm(i2) {
 				room.reset()
 				return
 			}
@@ -118,64 +117,76 @@ func (room *omok_room) message_handler() {
 	}
 }
 
-func (room *omok_room) victory_Confirm(index int) bool {
-	var cases int
-	for i := 1; i <= 4; i++ {
+func (room *OmokRoom) VictoryConfirm(index int) bool {
+	directions := []int{15, 1, 16, 14}
+	for _, direction := range directions {
 		count := 1
-		switch i {
-		case 1:
-			cases = 15
-		case 2:
-			cases = 1
-
-		case 3:
-			cases = 16
-
-		case 4:
-			cases = 14
-		}
-
 		for i := 1; i <= 4; i++ {
-			if 0 <= (cases*i)+index && (cases*i)+index < 225 && room.board_15x15[(cases*i)+index] == room.board_15x15[index] {
+			nextStoneIndex := (direction * i) + index
+			if 0 <= nextStoneIndex && nextStoneIndex < 225 && room.board_15x15[nextStoneIndex] == room.board_15x15[index] {
 				count++
 			} else {
 				break
 			}
 		}
 		for i := -1; i >= -4; i-- {
-			if 0 <= (cases*i)+index && (cases*i)+index < 225 && room.board_15x15[(cases*i)+index] == room.board_15x15[index] {
+			nextStoneIndex := (direction * i) + index
+			if 0 <= nextStoneIndex && nextStoneIndex < 225 && room.board_15x15[nextStoneIndex] == room.board_15x15[index] {
 				count++
 			} else {
 				break
 			}
 		}
 		if count >= 5 {
-			if room.board_15x15[index] == black {
-				room.uesr_1.writing("", "", "", "승리")
-				room.uesr_2.writing("", "", "", "패배")
-			} else {
-				room.uesr_1.writing("", "", "", "패배")
-				room.uesr_2.writing("", "", "", "승리")
-			}
+			room.SendVictoryMessage(room.board_15x15[index])
 			return true
 		}
 	}
 	return false
 }
 
+func (room *OmokRoom) SendVictoryMessage(winnerColor uint8) {
+	if winnerColor == black {
+		room.uesr_1.writing("", "", "", "승리")
+		room.uesr_2.writing("", "", "", "패배")
+
+	} else {
+		room.uesr_2.writing("", "", "", "승리")
+		room.uesr_1.writing("", "", "", "패배")
+	}
+}
+
 func (user *user) writing(d, c, y, m string) bool {
 	msg := Message{d, c, y, m}
 	if err := user.ws.WriteJSON(msg); err != nil {
 		log.Printf("conn.WriteMessage: %v", err)
-		return true
+		return false
 	}
-	return false
+	return true
 }
 
-func (room *omok_room) reset() {
+func (room *OmokRoom) reset() {
 	room.uesr_1.check = false
-	room.uesr_1.ws.Close()
 	room.uesr_2.check = false
-	room.uesr_2.ws.Close()
 	room.board_15x15 = [225]uint8{}
+	room.uesr_1.ws.Close()
+	room.uesr_2.ws.Close()
+}
+
+func ckk() {
+	for j := 0; true; j++ {
+		fmt.Printf("t:%d \n", j)
+		for i := 0; i < max; i++ {
+
+			if OmokRoomData[i].uesr_1.check {
+				fmt.Printf("room:%d ,1\n", i)
+
+			}
+			if OmokRoomData[i].uesr_2.check {
+				fmt.Printf("room:%d ,2\n", i)
+			}
+		}
+		fmt.Printf("\n\n")
+		time.Sleep(100000 * time.Microsecond)
+	}
 }
